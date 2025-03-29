ee88f60a30af5d331aa24297e9296baec1ff772d
anmoldeepcreative <anmoldeep.creative@gmail.com>
2025-03-29 22:55:26 +0530

    Login Feature and Dashboard changes

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import userRoutes from "./userRoutes"
-import approvedUserRoutes from "./approvedUserRoutes"
+import DashboardRoutes from "./DashboardRoutes"
 // import adminRoutes from "./adminRoutes"
 
 const router = express.Router();
 
 router.use("/users",userRoutes)
-router.use("/approved-user",approvedUserRoutes)
+router.use("/dashboard",DashboardRoutes)
 // router.use("/admin",adminRoutes)
 
 export default router;
